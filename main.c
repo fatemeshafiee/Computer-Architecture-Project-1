7552e62e309df27fb830d81ea9a114a506c19677
fatemeshafiee <ardestanifateme0@gmail.com>
2021-04-06 13:31:31 +0430

    first try to read a file line by line

--- main.c
+++ main.c
@@ -1,8 +1,22 @@
 #include <stdio.h>
-
-int main()
+#define MAX_LINE_LENGTH 100
+int main(int argc, char* argv[])
 {
+	FILE* Input;
+	fopen_s(&Input, argv[1], "r");
+	FILE* Output;
+	fopen_s(&Output,argv[2], "w");
+	char* instruction[65537];
+	while (fgets(instruction, MAX_LINE_LENGTH, Input))
+	{
 
+	}
+
+	int a = 10;
+
+	
+
+	
 
 	return 0;
 
