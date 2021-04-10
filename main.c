8f653c1b392bdbfae56c7b450e520cce3fd046e3
Parisa Asgarzadeh <pariss1380@gmail.com>
2021-04-11 00:04:49 +0430

    comments0

--- main.c
+++ main.c
@@ -1,5 +1,35 @@
-#include <stdio.h>
+﻿#include <stdio.h>
+#include <stdlib.h>
+
 #define MAX_LINE_LENGTH 100
+
+//16 رجیستر داریم
+// مقدار رجیستر اول همیشه صفر است.
+// رجیسترها سی و دو بیتی هستند
+//فعلا نمیدونم بهتره استرینگ باشن یا اینت باشن پس از هردو تعریفشون میکنم.
+
+char* char_Register[16];
+unsigned long int int_Register[16];
+
+//یک سری برچسب داریم که باید سری اول، تشخیص بدیمشون
+// بعد بریزیمشون تو
+// symbol table
+// به همراه آدرسشون
+struct MAP_lable
+{
+	char* lable;
+	int address;
+};
+
+
+symbol_Table = (struct MAP_lable*)malloc();
+
+//طبق داک، این کار رو تو اسکن اول داریم انجام میدیم
+void first_scan(FILE* filePointer)
+{
+
+}
+
 int main(int argc, char* argv[])
 {
 	FILE* Input;
@@ -11,12 +41,6 @@ int main(int argc, char* argv[])
 	{
 
 	}
-
-	int a = 10;
-
-	
-
-	
 
 	return 0;
 
